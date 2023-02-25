4ce38a176ee8a7a0b1736e9edf2fe8ebec4956fb
Phillip Burleigh <pburle2@illinois.edu>
2023-02-24 20:45:28 -0600

    Add doppler connection

--- webpage/index.js
+++ webpage/index.js
@@ -5,7 +5,24 @@ const router = express.Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
-const fs = require('fs')
+const fs = require('fs');
+
+// Load secrets
+const secrets = JSON.parse(require('child_process').execSync('node doppler-secrets.js'));
+
+
+// MongoDB Atlas Connection Code
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dbPass = secrets.MONGO_DB_PASS;
+const uri = "mongodb+srv://hackbird23:" + dbPass + "@birdcluster0.ttenopu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Test DB connection
+client.connect(err => {
+  const collection = client.db("sample_mflix").collection("comments");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(bodyParser.json());
 
